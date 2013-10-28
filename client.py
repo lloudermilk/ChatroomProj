dc9886ae1320df9563cfe46e5abddac33be3b635
Eric Wooley <5wooley4@gmail.com>
2013-10-28 15:59:36 -0700

    Updated server and client

--- client.py
+++ client.py
@@ -1,11 +1,53 @@
-#!/usr/bin/python           # This is client.py file
 
-import socket               # Import socket module
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 12345                # Reserve a port for your service.
 
-s.connect((host, port))
-print s.recv(1024)
-s.close                     # Close the socket when done
+"""
+An example client. Run simpleserv.py first before running this.
+"""
+
+from twisted.internet import reactor, protocol
+
+
+# a client protocol
+
+class EchoClient(protocol.Protocol):
+    """Once connected, send a message, then print the result."""
+    
+    def connectionMade(self):
+    	print "Connecting to server..."
+    	message = raw_input("Enter message: ")
+    	while message != "exit":
+        	self.transport.write(message)
+        	message = raw_input("New Message: ")
+    
+    def dataReceived(self, data):
+        "As soon as any data is received, write it back."
+        print "Server said:", data
+        self.transport.loseConnection()
+    
+    def connectionLost(self, reason):
+        print "connection lost"
+
+class EchoFactory(protocol.ClientFactory):
+    protocol = EchoClient
+
+    def clientConnectionFailed(self, connector, reason):
+        print "Connection failed - goodbye!"
+        reactor.stop()
+    
+    def clientConnectionLost(self, connector, reason):
+        print "Connection lost - goodbye!"
+        reactor.stop()
+
+
+# this connects the protocol to a server runing on port 8000
+def main():
+    f = EchoFactory()
+    reactor.connectTCP("localhost", 8000, f)
+    reactor.run()
+
+# this only runs if the module was *not* imported
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,12 +1,27 @@
-from twisted.internet import protocol, reactor
+
+# Copyright (c) Twisted Matrix Laboratories.
+# See LICENSE for details.
+
+
+from twisted.internet import reactor, protocol
+
 
 class Echo(protocol.Protocol):
+    """This is just about the simplest possible protocol"""
+    
     def dataReceived(self, data):
-        self.transport.write(data)
+        "As soon as any data is received, write it back."
+        print "Got Message", data
+        self.transport.write("You said",data)
 
-class EchoFactory(protocol.Factory):
-    def buildProtocol(self, addr):
-        return Echo()
 
-reactor.listenTCP(12345, EchoFactory())
-reactor.run()
+def main():
+    """This runs the protocol on port 8000"""
+    factory = protocol.ServerFactory()
+    factory.protocol = Echo
+    reactor.listenTCP(8000,factory)
+    reactor.run()
+
+# this only runs if the module was *not* imported
+if __name__ == '__main__':
+    main()
